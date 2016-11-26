e211b0f174a9ea03efd72cf4ed93e85c1ee07582
alexandraklein <ali.klein@deutschinc.com>
2016-11-26 10:09:37 -0800

    Parallax effect.

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,69 @@
 $(function() {
-    /* Countdown */
-    $("#counter").countdown('2016/08/24 10:00:00', function(event)  {
-        $(this).html(event.strftime(''
-            + '<span>%D</span> '
-            + '<span>%H</span> '
-            + '<span>%M</span> '
-            + '<span>%S</span>'));
+
+    var w = $(window).width(),
+        h = $(window).height(),
+        $section = $("section");
+
+    function Createparallaxbg(parallaxImage) {
+        var ParSecImg = $(parallaxImage).attr("data-image");
+        $(parallaxImage).attr("style", "background-image:url(" + ParSecImg + ");");
+    }
+
+    function parallaxbg(position, parallaxImage) {
+        var currentTop = $(window).scrollTop();
+        var ParSecPT = $(position).position().top;
+        if (currentTop > (ParSecPT - h)) {
+
+            $(parallaxImage).css({
+                "background-position": "center " + ((currentTop - ParSecPT) / 2 - (h/5)) + "px"
+            });
+
+        }
+    }
+
+    function initScroll() {
+        if($section.hasClass("parallax1")){
+            parallaxbg(".parallax1",".parallax1");
+        }
+        if($section.hasClass("parallax2")){
+            parallaxbg(".parallax2",".parallax2");
+        }
+        if($section.hasClass("parallax3")){
+            parallaxbg(".parallax3",".parallax3");
+        }
+        if($section.hasClass("parallax4")){
+            parallaxbg(".parallax4",".parallax4");
+        }
+        if($section.hasClass("parallax5")){
+            parallaxbg(".parallax5",".parallax5");
+        }
+    }
+
+    function init() {
+        if ($section.hasClass("parallax1")) {
+            Createparallaxbg(".parallax1");
+        }
+        if ($section.hasClass("parallax2")) {
+            Createparallaxbg(".parallax2");
+        }
+        if ($section.hasClass("parallax3")) {
+            Createparallaxbg(".parallax3");
+        }
+        if ($section.hasClass("parallax4")) {
+            Createparallaxbg(".parallax4");
+        }
+        if ($section.hasClass("parallax5")) {
+            Createparallaxbg(".parallax5");
+        }
+    }
+
+
+
+    init();
+
+    $(window).bind('scroll', function () {
+        initScroll();
     });
 
-    /* Cheerio Parallax */
-    $(window).on('scroll', function () {
-        window.requestAnimationFrame(function () {
-            var scrolled = $(window).scrollTop();
-            $('.cheerio').css({
-                'transform': 'translate3d(0,' + scrolled * -0.64 + 'px, 0)'
-            });
-            $('.cheerio:nth-of-type(2)').css({
-                'transform': 'translate3d(0,' + scrolled * -0.42 + 'px, 0)'
-            });
-            $('.cheerio:nth-of-type(3)').css({
-                'transform': 'translate3d(0,' + scrolled * -0.23 + 'px, 0)'
-            });
-            $('.cheerio:nth-of-type(4)').css({
-                'transform': 'translate3d(0,' + scrolled * -0.84 + 'px, 0)'
-            });
-        });
 
-    });
 });
