55bf0cafd496c6bc2634a74e4b0a461bb648313d
alexandraklein <ali.klein@deutschinc.com>
2016-11-26 14:14:30 -0800

    parallax food images

--- app/js/main.js
+++ app/js/main.js
@@ -1,65 +1,47 @@
 $(function() {
 
-    var w = $(window).width(),
-        h = $(window).height(),
-        $section = $("section");
+    var h = $(window).height(),
+        $section = $('section');
 
-    function Createparallaxbg(parallaxImage) {
-        var ParSecImg = $(parallaxImage).attr("data-image");
-        $(parallaxImage).attr("style", "background-image:url(" + ParSecImg + ");");
+    function Createparallaxbg(parallaxImg) {
+
+        var bgImg = $(parallaxImg).attr('data-image');
+        $(parallaxImg).attr('style', 'background-image:url(' + bgImg + ');');
+
     }
 
-    function parallaxbg(position, parallaxImage) {
-        var currentTop = $(window).scrollTop();
-        var ParSecPT = $(position).position().top;
-        if (currentTop > (ParSecPT - h)) {
+    function parallaxbg(position, parallaxImg) {
 
-            $(parallaxImage).css({
-                "background-position": "center " + ((currentTop - ParSecPT) / 2 - (h/5)) + "px"
+        var positionTop = $(window).scrollTop();
+        var imgPosition = $(position).position().top;
+
+        if (positionTop > (imgPosition - h)) {
+            $(parallaxImg).css({
+                'background-position': 'center ' + ((positionTop - imgPosition) / 2 - (h/5)) + 'px'
             });
-
         }
     }
 
     function initScroll() {
-        if($section.hasClass("parallax1")){
-            parallaxbg(".parallax1",".parallax1");
-        }
-        if($section.hasClass("parallax2")){
-            parallaxbg(".parallax2",".parallax2");
-        }
-        if($section.hasClass("parallax3")){
-            parallaxbg(".parallax3",".parallax3");
-        }
-        if($section.hasClass("parallax4")){
-            parallaxbg(".parallax4",".parallax4");
-        }
-        if($section.hasClass("parallax5")){
-            parallaxbg(".parallax5",".parallax5");
-        }
+
+        $section.each(function(idx) {
+            if ($(this).hasClass('parallax' + (idx + 1))) {
+                parallaxbg('.parallax' + (idx + 1), '.parallax' + (idx + 1));
+            }
+        });
     }
 
     function init() {
-        if ($section.hasClass("parallax1")) {
-            Createparallaxbg(".parallax1");
-        }
-        if ($section.hasClass("parallax2")) {
-            Createparallaxbg(".parallax2");
-        }
-        if ($section.hasClass("parallax3")) {
-            Createparallaxbg(".parallax3");
-        }
-        if ($section.hasClass("parallax4")) {
-            Createparallaxbg(".parallax4");
-        }
-        if ($section.hasClass("parallax5")) {
-            Createparallaxbg(".parallax5");
-        }
+
+        $section.each(function(idx) {
+            if ($(this).hasClass('parallax' + (idx + 1))) {
+                Createparallaxbg('.parallax' + (idx + 1));
+            }
+        });
     }
 
-
-
     init();
+    initScroll();
 
     $(window).bind('scroll', function () {
         initScroll();
